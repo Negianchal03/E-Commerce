4866193e810197a0ce1f2f9eb85aaaebcff85d49
Negianchal03 <negianchal983@gmail.com>
2026-01-12 18:50:01 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,269 @@
+// console.log("hey!");
+// console.log("34+78");
+// alert("Are you sure you want exit");
+// prompt("What is ypur name?");
+// confirm("Do you know how to drive?");
+// document.write("hello everyOne this is my first class of javaScript");
+
+// camelCase- recommended
+// var firstName = "Anchal";
+
+// kabab Case
+// var first-name = "Anchal"
+
+// snake Case
+// var first_name = "Anchal"
+
+// Pascal Case
+// var FirstName = "Anchal"
+
+
+
+
+
+// var firstName = "Abhishek";
+// var age = 23;
+// var city = "Dehradun";
+// console.log(firstName , age);
+
+// let firstName = "Anchal";
+// let age = 22;
+// let city = "Dehradun";
+// console.log(firstName , age , city);
+
+// const firstName = "Aditya";
+// const age = 21;
+// const city = "Dehradun";
+// console.log(firstName , age , city);
+
+
+
+
+// var
+// Global scope/ function scope
+// {
+//     var a =5;
+// }
+
+// console.log(a)
+
+// function add(){
+//     var b =7
+// }
+
+// console.log(b)
+
+// you can redeclare it
+// var a =5;
+// var a =6;
+// console.log(a)
+
+// we can reassign/update it
+// var a =5;
+//  a =6;
+// console.log(a)
+
+
+
+
+
+// let
+// block scope
+// {
+//     let a =6;
+// }
+
+// console.log(a)
+
+// we cannot redeclare it
+// let a =5;
+// let a =6;
+
+// we can reassign/update it
+// let a =5;
+//  a =7;
+//  console.log(a)
+
+
+
+
+
+
+// const
+// block scope
+// {
+//     const a = 7;
+//     console.log(a)
+// }
+
+// we cannot redeclare it
+// const a =6;
+// const a =7;
+
+// we cannot reassign/update it
+// const a =7;
+// a =8;
+// console.log(a)
+
+
+
+
+
+
+
+
+
+// Primitive
+// number
+// let age = 21;
+// let height = 8;
+// console.log(age,height)
+// console.log(typeof age);
+
+// string
+// let name = "Aditya"
+// let age = 21;
+// console.log(name,age)
+// console.log(typeof age);
+
+// boolean
+// let isUserExit = true;
+// console.log(isUserExit);
+// console.log(typeof isUserExit);
+
+// BigInt
+// let num = 7878278272275767n
+// console.log(num)
+// console.log(typeof num);
+
+// undefined
+// let a;
+// console.log(a)
+// console.log(typeof a);
+
+// null
+// let a = null;
+// console.log(a)
+// console.log(typeof a);
+
+// symbol
+// let id1 = symbol("id");
+// let id2 = symbol("id");
+// console.log(id1==id2);
+// console.log(typeof id1);
+
+// template literals ES-6
+// console.log(`Himanshu ${age} ${name}`)
+
+
+// Non primitive
+
+// OBJECT
+// let student = {
+//     name: "ashish",
+//     age: 24,
+//     batch: "cyber security",
+// };
+// console.log(student);
+// console.log(typeof student);
+
+// Array
+// let arr =[1,2,3,"Anchal","Akriti",true,false];
+// console.log(typeof arr)
+// console.log(arr)
+
+// function
+// function sum(){
+//     console.log("hi");
+// }
+// sum();
+
+// Premitive
+// let a =5;
+// a = 6;
+// let b = a;
+// b = a + 6;
+// console.log(b,a);
+
+// Non Premitive
+// let obj ={
+//     name:"Abhishek",
+//     age: 23,
+//     ciy: "Dehradun",
+// };
+
+// let obj2 = obj;
+// obj2.city = "Delhi";
+
+// console.log(obj2);
+// console.log(obj);
+
+
+// Premitive
+// immutable
+// store single value
+// directly copy
+
+// Non Premitive
+// mutable
+// store multiple value
+// we cannot directly copy
+
+
+
+// Arithmetic Operator
+// let a =5;
+// let b =4;
+
+// Binary- single operator + 2 Operands
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a % b);
+// console.log(a / b);
+// console.log(a ** b);
+
+/// Unary
+// let a = 5;
+
+// pre increment
+// console.log(++a);
+
+// post increment
+// console.log(a++);
+// console.log(a)
+
+// pre decrement
+// console.log(--a);
+
+// post decrement
+// console.log(a--);
+// console.log(a)
+
+console.log("hi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
